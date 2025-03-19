d65fc0eb64811fdaa0c7d5d8b22f0663960516aa
Samero0 <alex.fr.estudio@gmail.com>
2025-03-19 11:46:23 +0000

    date format fix

--- src/hooks/formatDate.ts
+++ src/hooks/formatDate.ts
@@ -6,7 +6,7 @@ export const formatDate = (date : Date | null) =>{
         const day = String(date.getDate()).padStart(2, '0'); // ensure two digits
         const month = String(date.getMonth() + 1).padStart(2, '0'); // ensure two digits
         const year = date.getFullYear();
-        return `${day}-${month}-${year}`;  //format as "DD-MM-YYYY"
+        return `${day}/${month}/${year}`;  //format as "DD-MM-YYYY"
     }
 }
 
